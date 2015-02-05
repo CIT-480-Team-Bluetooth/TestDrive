d9ad11cc5a1bbd2ec8f46a5bd1d107cca2e94c7c
Jeffrey Quesnelle <jquesnelle@gmail.com>
2015-02-05 03:03:53 -0500

    initial design with tabs and a few screens

--- app/src/main/java/edu/oakland/secs/testdrive/MainActivity.java
+++ app/src/main/java/edu/oakland/secs/testdrive/MainActivity.java
@@ -1,94 +1,50 @@
 package edu.oakland.secs.testdrive;
 
 import android.app.Activity;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.os.Build;
+
+import junit.framework.Test;
 
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends ActionBarActivity {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+    private SlidingTabLayout mSlidingTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new TestDrivePageAdapter(getSupportFragmentManager()));
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setViewPager(mViewPager);
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
@@ -106,44 +62,41 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    public static final int FRAGMENT_VEHICLE = 0;
+    public static final int FRAGMENT_RECORD = 1;
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+    public class TestDrivePageAdapter extends FragmentPagerAdapter {
+
+        public TestDrivePageAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        public PlaceholderFragment() {
+        public Fragment getItem(int position) {
+            switch(position) {
+                case FRAGMENT_VEHICLE:
+                    return new VehicleFragment();
+                case FRAGMENT_RECORD:
+                    return new RecordFragment();
+                default:
+                    return null;
+            }
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        public int getCount() {
+            return 2;
         }
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case FRAGMENT_VEHICLE:
+                    return getString(R.string.vehicle);
+                case FRAGMENT_RECORD:
+                    return getString(R.string.record);
+                default:
+                    return null;
+            }
         }
+
     }
 
 }
---
+++ app/src/main/java/edu/oakland/secs/testdrive/RecordFragment.java
@@ -0,0 +1,66 @@
+package edu.oakland.secs.testdrive;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by jeffq on 2/5/2015.
+ */
+public class RecordFragment extends Fragment {
+
+    private Spinner mWeatherSpinner;
+    private Spinner mRoadTypeSpinner;
+    private Spinner mRoadConditionSpinner;
+    private Spinner mVisibilitySpinner;
+    private Spinner mTrafficSpinner;
+    private ArrayAdapter<CharSequence> mWeatherAdapter;
+    private ArrayAdapter<CharSequence> mRoadTypeAdapter;
+    private ArrayAdapter<CharSequence> mRoadConditionAdapter;
+    private ArrayAdapter<CharSequence> mVisibilityAdapter;
+    private ArrayAdapter<CharSequence> mTrafficAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mWeatherSpinner = (Spinner)view.findViewById(R.id.weather_spinner);
+        mWeatherAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.weather_items, android.R.layout.simple_spinner_item);
+        mWeatherAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mWeatherSpinner.setAdapter(mWeatherAdapter);
+
+        mRoadTypeSpinner = (Spinner)view.findViewById(R.id.road_type_spinner);
+        mRoadTypeAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.road_type_items, android.R.layout.simple_spinner_item);
+        mRoadTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mRoadTypeSpinner.setAdapter(mRoadTypeAdapter);
+
+        mRoadConditionSpinner = (Spinner)view.findViewById(R.id.road_conditions_spinner);
+        mRoadConditionAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.road_condition_items, android.R.layout.simple_spinner_item);
+        mRoadTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mRoadConditionSpinner.setAdapter(mRoadConditionAdapter);
+
+        mVisibilitySpinner = (Spinner)view.findViewById(R.id.visibility_spinner);
+        mVisibilityAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.visibility_items, android.R.layout.simple_spinner_item);
+        mVisibilityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mVisibilitySpinner.setAdapter(mVisibilityAdapter);
+
+        mTrafficSpinner = (Spinner)view.findViewById(R.id.traffic_spinner);
+        mTrafficAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.traffic_items, android.R.layout.simple_spinner_item);
+        mTrafficAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mTrafficSpinner.setAdapter(mTrafficAdapter);
+    }
+
+}
